ee109ebb15c15e8562a14b3ea9563f010d5baba3
rsleeper1 <39035524+rsleeper1@users.noreply.github.com>
2019-05-29 23:29:59 -0500

    Update week4Client.py

--- week4Client.py
+++ week4Client.py
@@ -6,7 +6,6 @@ port = 9500
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((host, port))
     s.sendall(b"Hello")
-    s.sendall(b"Test")
     data = s.recv(1024)
 
 print('Received', repr(data))
